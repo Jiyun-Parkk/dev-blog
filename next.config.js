4327ad02410af69ef80fa2ccb7865067631452a7
박지윤 <jyun1937@naver.com>
2022-12-09 16:02:24 +0900

    Docs: webpack 설정 변경

--- next.config.js
+++ next.config.js
@@ -73,13 +73,17 @@ module.exports = () => {
         },
       ]
     },
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-
-      return config
+    webpack: (config, { dev, isServer }) => {
+      ;(config.resolve.alias = {
+        ...config.resolve.alias,
+        // fixes next-mdx-remote: Package path ./jsx-runtime.js is not exported from package react
+        // https://github.com/hashicorp/next-mdx-remote/issues/237
+        'react/jsx-runtime.js': require.resolve('react/jsx-runtime'),
+      }),
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        })
     },
   })
 }

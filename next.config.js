0c6893f971571108971084b24cfeb2498953866b
박지윤 <jyun1937@naver.com>
2022-12-09 21:05:44 +0900

    fix:csp 수정 newsletter component 삭제

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.june-dev.vercel.app/ github.com/jiyun-par/dev-blog;
+  default-src 'self' data: gap: https://ssl.gstatic.com ;
+  script-src 'self' https://june-dev.vercel.app/  'unsafe-eval' 'unsafe-inline' giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';

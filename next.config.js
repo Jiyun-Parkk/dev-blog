aaca00ceaff8251c59bd9de402ab2b2795a1b9cf
박지윤 <jyun1937@naver.com>
2022-12-09 21:16:00 +0900

    docs:csp domain 변경

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self' ;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.googletagmanager.com *.google-analytics.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com; ;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app ;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
